a098e93d359607204927d18e19f9c65a19cead3d
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-05-11 13:55:16 +0300

    0.0.2 Loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,45 @@
-import { useDeep } from '@deep-foundation/deeplinks/imports/client';
+import { Box, CircularProgress, CircularProgressLabel, Heading, SimpleGrid } from '@chakra-ui/react';
 import CytoGraph from '@deep-foundation/deepcase/imports/cyto/graph';
-import { useTranslation } from 'next-i18next';
-import { useRouter } from 'next/router';
-import { i18nGetStaticProps } from '../src/i18n';
+import { useRefstarter } from '@deep-foundation/deepcase/imports/refstater';
+import { useDeep } from '@deep-foundation/deeplinks/imports/client';
+import { Id, Link } from '@deep-foundation/deeplinks/imports/minilinks';
 import {
   DeviceProvider,
   useDevice
 } from '@deep-foundation/deepmemo-imports/imports/device';
-import Test from '@deep-foundation/deepmemo-imports/imports/index';
+import { useTranslation } from 'next-i18next';
+import { useRouter } from 'next/router';
+import React, { useEffect, useRef, useState } from 'react';
 import { Connection } from '../src/connection';
-import { Id, Link } from '@deep-foundation/deeplinks/imports/minilinks';
-import { useRef } from 'react';
-import { useRefstarter } from '@deep-foundation/deepcase/imports/refstater';
-import { Box, Heading, SimpleGrid } from '@chakra-ui/react';
+import { i18nGetStaticProps } from '../src/i18n';
 
-function DeviceView() {
+const Loading = React.memo(function Loading({ factor, interval }: { factor: any, interval: number }) {
+  const [value, setValue] = useState(0);
+  const [synced, setSynced] = useState(false);
+  useEffect(() => {
+    setValue(0);
+    setSynced(true);
+    const i = setInterval(() => setValue(v => {
+      if (v > (interval / 2)) setSynced(false);
+      return v > interval ? 0 : v + (interval / 100);
+    }), interval / 100);
+    return () => clearInterval(i);
+  }, [factor]);
+  return <CircularProgress value={(value / interval) * 100} size="1em" color={synced ? 'green.400' : 'blue.400'}>
+    <CircularProgressLabel fontSize={'0.5em'} color={synced ? 'black' : 'gray.500'}>{synced ? '✔' : '✗'}</CircularProgressLabel>
+  </CircularProgress>;
+});
+
+function DeviceView({ interval }: { interval: number }) {
   const device = useDevice();
   const deep = useDeep();
   const cyRef = useRef();
   const cytoViewportRef = useRefstarter<{ pan: { x: number; y: number; }; zoom: number }>();
   const { data: containTree } = deep.useDeepId('@deep-foundation/core', 'containTree');
   const { data: links = [] }: { data?: Link<Id>[] } = deep.useDeepSubscription({ up: { parent_id: device?.id || 0, tree_id: containTree || 0 } });
+
   return <>
-    <Heading>Device {device?.id ? device?.id : 'syncing'}</Heading>
+    <Heading>Device {device?.id ? <>{device?.id} <Loading factor={device} interval={interval}/></> : <>{'syncing'}</>}</Heading>
     <SimpleGrid columns={{sm: 1, md: 2}}>
       <Box><pre>{JSON.stringify(device, null, 2)}</pre></Box>
       {!!device?.id && <Box w={500} h={500} border={'1px'} rounded='md' position="relative">
@@ -44,8 +61,8 @@ export default function Page() {
   return (<>
     <Connection/>
     {!!deep?.linkId && <>
-      <DeviceProvider containerId={deep.linkId}>
-        <DeviceView/>
+      <DeviceProvider containerId={deep.linkId} interval={5000}>
+        <DeviceView interval={5000}/>
       </DeviceProvider>
     </>}
   </>);
