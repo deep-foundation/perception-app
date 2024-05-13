d1d9124a2c1df952f2d52fa7f6a92362d5eaedf0
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-05-13 04:34:27 +0300

    0.0.3 geolocation and sliders

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,11 @@
-import { Box, CircularProgress, CircularProgressLabel, Heading, SimpleGrid } from '@chakra-ui/react';
+import {
+  Box, Button, CircularProgress, CircularProgressLabel, Heading, SimpleGrid,
+  Slider,
+  SliderFilledTrack,
+  SliderMark,
+  SliderThumb,
+  SliderTrack,
+} from '@chakra-ui/react';
 import CytoGraph from '@deep-foundation/deepcase/imports/cyto/graph';
 import { useRefstarter } from '@deep-foundation/deepcase/imports/refstater';
 import { useDeep } from '@deep-foundation/deeplinks/imports/client';
@@ -7,8 +14,10 @@ import {
   DeviceProvider,
   useDevice
 } from '@deep-foundation/deepmemo-imports/imports/device';
-import { useTranslation } from 'next-i18next';
-import { useRouter } from 'next/router';
+import {
+  GeolocationProvider,
+  useGeolocation
+} from '@deep-foundation/deepmemo-imports/imports/geolocation';
 import React, { useEffect, useRef, useState } from 'react';
 import { Connection } from '../src/connection';
 import { i18nGetStaticProps } from '../src/i18n';
@@ -30,15 +39,43 @@ const Loading = React.memo(function Loading({ factor, interval }: { factor: any,
   </CircularProgress>;
 });
 
-const Graph = React.memo(function Graph() {
+const Interval = React.memo(function Loading({ value, onChange }: { value: any, onChange: any }) {
+  return <Box p={4} pt={6}><Slider value={value / 100} onChange={(v) => onChange(v * 100)}>
+    <SliderMark value={25} mt={'2'} ml={'-2.5'} fontSize={'sm'}>
+      2500
+    </SliderMark>
+    <SliderMark value={50} mt={'2'} ml={'-2.5'} fontSize={'sm'}>
+      5000
+    </SliderMark>
+    <SliderMark value={75} mt={'2'} ml={'-2.5'} fontSize={'sm'}>
+      7500
+    </SliderMark>
+    <SliderMark
+      value={value / 100}
+      textAlign='center'
+      bg='blue.500'
+      color='white'
+      mt='-10'
+      ml='-7'
+      w='15'
+    >
+      {value}ms
+    </SliderMark>
+    <SliderTrack>
+      <SliderFilledTrack />
+    </SliderTrack>
+    <SliderThumb />
+  </Slider></Box>;
+});
+
+const Graph = React.memo(function Graph({ linkId }: { linkId: Id }) {
   const deep = useDeep();
-  const device = useDevice();
   const cyRef = useRef();
   const cytoViewportRef = useRefstarter<{ pan: { x: number; y: number; }; zoom: number }>();
   const { data: containTree } = deep.useDeepId('@deep-foundation/core', 'containTree');
-  const { data: links = [] }: { data?: Link<Id>[] } = deep.useDeepSubscription({ up: { parent_id: device?.id || 0, tree_id: containTree || 0 } });
+  const { data: links = [] }: { data?: Link<Id>[] } = deep.useDeepSubscription({ up: { parent_id: linkId || 0, tree_id: containTree || 0 } });
   return <>
-    {!!device?.id && <Box w={500} h={500} border={'1px'} rounded='md' position="relative">
+    {!!linkId && <Box w={500} h={500} border={'1px'} rounded='md' position="relative">
       {deep?.linkId && <CytoGraph links={links} cyRef={cyRef} cytoViewportRef={cytoViewportRef}/>}
     </Box>}
   </>;
@@ -48,28 +85,48 @@ const DeviceView = React.memo(function DeviceView({ interval }: { interval: numb
   const deep = useDeep();
   const device = useDevice();
   return <>
-    <Heading>Device {device?.id ? <>{device?.id} <Loading factor={device} interval={interval}/></> : <>{'syncing'}</>}</Heading>
+    <Heading>Device {device?.id ? <>{device?.id}</> : <>{'syncing'}</>} <Loading factor={device} interval={interval}/></Heading>
     <SimpleGrid columns={{sm: 1, md: 2}}>
       <Box><pre>{JSON.stringify(device, null, 2)}</pre></Box>
-      {deep?.linkId && <Graph/>}
+      {deep?.linkId && device?.id && <Graph linkId={device.id}/>}
     </SimpleGrid>
   </>;
-})
+});
+
+const GeolocationView = React.memo(function GeolocationView({ interval }: { interval: number }) {
+  const deep = useDeep();
+  const geolocation = useGeolocation();
+  return <>
+    <Heading>Geolocation <><Loading factor={geolocation.position} interval={interval}/></></Heading>
+    <Button onClick={() => geolocation.request()}>request</Button>
+    <SimpleGrid columns={{sm: 1, md: 2}}>
+      <Box><pre>{JSON.stringify(geolocation, null, 2)}</pre></Box>
+    </SimpleGrid>
+  </>;
+});
 
 export default function Page() {
   const deep = useDeep();
-  const { t } = useTranslation();
-  const router = useRouter();
 
   // @ts-ignore
   if (typeof(window) === 'object') window.deep = deep;
   console.log('deep', deep);
 
+  const [deviceInterval, setDeviceInterval] = useState(5000);
+  const [geolocationInterval, setGeolocationInterval] = useState(5000);
+
   return (<>
     <Connection/>
-    <DeviceProvider containerId={deep?.linkId} interval={5000}>
-      <DeviceView interval={5000}/>
-    </DeviceProvider>
+    <Box p={4}>
+      <DeviceProvider containerId={deep?.linkId} interval={deviceInterval}>
+        <GeolocationProvider containerId={deep?.linkId} interval={geolocationInterval}>
+          <Interval value={deviceInterval} onChange={setDeviceInterval}/>
+          <DeviceView interval={deviceInterval}/>
+          <Interval value={geolocationInterval} onChange={setGeolocationInterval}/>
+          <GeolocationView interval={geolocationInterval}/>
+        </GeolocationProvider>
+      </DeviceProvider>
+    </Box>
   </>);
 }
 
