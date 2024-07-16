2a1fac2b85266317482c26f2885967c748f4f008
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-07-16 11:45:19 +0000

    0.0.13

--- imports/link.tsx
+++ imports/link.tsx
@@ -1,17 +1,20 @@
 import {
   Box,
-  Button
+  Button,
+  Heading,
+  Text,
 } from '@chakra-ui/react';
+import { useDeep } from "@deep-foundation/deeplinks/imports/client";
 import { Id } from '@deep-foundation/deeplinks/imports/minilinks';
 
 export const LinkButton = ({
-  id, name, type, icon, isActive, onClick,
+  id, name: _name, type: _type, icon, isActive, onClick,
   buttonRef,
   ...props
 }: {
   id: Id;
   name?: string;
-  type: string;
+  type?: string;
   icon: string;
   isActive: boolean;
   onClick?: (id: Id) => void;
@@ -20,20 +23,40 @@ export const LinkButton = ({
 
   [key: string]: any;
 }) => {
+  const deep = useDeep();
+  const link = deep?.minilinks?.byId[id];
+  const name = _name || deep.nameLocal(id);
+  const type = _type || deep.nameLocal(link?.type_id);
   return <Button
     ref={buttonRef}
-    h='3em' variant={isActive ? 'active' : 'solid'}
+    variant={isActive ? 'active' : 'solid'}
     onClick={(e) => {
       e.stopPropagation();
       e.preventDefault();
       onClick && onClick(id);
     }}
     justifyContent={'left'}
+    h='auto' pt={2} pb={2}
     {...props}
   >
-    {icon} <Box textAlign='left' pl='0.5em'>
+    {icon} <Box textAlign='left' pl='0.5em' w='100%' overflow='hidden'>
       {!!((name) || (type)) && <Box fontSize="sm">{name || type}</Box>}
       <Box fontSize="xxs">{name ? type : ''} {id}</Box>
+      {!!props?.children && props.children}
+      {!!link?.value && <>
+        <Text fontSize="xxs" opacity={0.5}>value:</Text>
+        <Text fontSize="xs">
+          {typeof(link?.value?.value) == 'string' && <>
+            <Heading as='pre' fontSize='xs' noOfLines={1} w='100%'>{link?.value?.value}</Heading>
+          </>}
+          {typeof(link?.value?.value) == 'number' && <>
+            <Heading as='pre' fontSize='xs' noOfLines={1} w='100%'>{link?.value?.value}</Heading>
+          </>}
+          {typeof(link?.value?.value) == 'object' && <>
+            <Heading as='pre' fontSize='xs' noOfLines={1} w='100%'>{JSON.stringify(link?.value?.value)}</Heading>
+          </>}
+        </Text>
+      </>}
     </Box>
   </Button>;
 };
