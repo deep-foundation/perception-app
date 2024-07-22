9d1ddc4b9be42d050f049acd30d2fbf7776fb247
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-07-22 13:06:46 +0000

    sync version fix

--- sync-version.js
+++ sync-version.js
@@ -2,5 +2,5 @@ const fs = require('fs');
 const path = require('path');
 const nextPckg = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8'));
 const electronPckg = JSON.parse(fs.readFileSync(path.join(__dirname, 'electron', 'package.json'), 'utf8'));
-electronPckg.id = nextPckg.id;
+electronPckg.version = nextPckg.version;
 fs.writeFileSync(path.join(__dirname, 'electron', 'package.json'), JSON.stringify(electronPckg));
