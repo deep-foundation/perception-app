bbc8a3cd3ea092aaf274cd3acfe186e1fae6b562
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-07-22 13:20:15 +0000

    0.0.24

--- sync-version.js
+++ sync-version.js
@@ -3,4 +3,4 @@ const path = require('path');
 const nextPckg = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8'));
 const electronPckg = JSON.parse(fs.readFileSync(path.join(__dirname, 'electron', 'package.json'), 'utf8'));
 electronPckg.version = nextPckg.version;
-fs.writeFileSync(path.join(__dirname, 'electron', 'package.json'), JSON.stringify(electronPckg));
+fs.writeFileSync(path.join(__dirname, 'electron', 'package.json'), JSON.stringify(electronPckg, null, 2));
