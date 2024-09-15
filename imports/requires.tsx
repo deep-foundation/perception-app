8dcbc3f59df9799e830be11cf5d352afc086db64
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-09-15 14:36:46 +0000

    0.0.81

--- imports/requires.tsx
+++ imports/requires.tsx
@@ -1,9 +1,13 @@
-import { requires as piRequires } from '@deep-foundation/perception-imports'
+import { requires as piRequires } from '@deep-foundation/perception-imports';
 
 // @ts-ignore
 const GraphQL = dynamic(() => import('./graphql').then(m => m.GraphQL), { ssr: false })
 
 export const requires: any = {
-  ...piRequires
+  ...piRequires,
+  '@deep-foundation/perception-app': {
+    ...piRequires['@deep-foundation/perception-imports'],
+    GraphQL,
+  },
 };
 
